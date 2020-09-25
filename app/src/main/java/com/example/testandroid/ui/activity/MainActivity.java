bf40c8e49f3001c616199b88096b2a15bf27be3a
fanming <fanming_183@163.com>
2020-09-25 17:52:09 +0800

    刷新加载

--- app/src/main/java/com/example/testandroid/ui/activity/MainActivity.java
+++ app/src/main/java/com/example/testandroid/ui/activity/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -12,6 +13,7 @@ import com.example.testandroid.api.ILoadStudentListener;
 import com.example.testandroid.bean.StudentResponse;
 import com.example.testandroid.manager.StudentManager;
 import com.example.testandroid.ui.adapter.StudentAdapter;
+import com.scwang.smartrefresh.layout.SmartRefreshLayout;
 
 import java.util.List;
 import java.util.logging.Logger;
@@ -21,24 +23,31 @@ public class MainActivity extends AppCompatActivity  implements ILoadStudentList
     private static final Logger logger = Logger.getLogger(MainActivity.class.getName());
 
     private StudentAdapter studentAdapter;
+    private SmartRefreshLayout smartRefreshLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        smartRefreshLayout = findViewById(R.id.smart_refresh_layout);
         RecyclerView recyclerView = findViewById(R.id.recycler_view);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         studentAdapter =  new StudentAdapter();
         recyclerView.setAdapter(studentAdapter);
+        recyclerView.addItemDecoration(new DividerItemDecoration(this,DividerItemDecoration.VERTICAL));
 
         StudentManager.getInstance().refresh(this);
-
+        smartRefreshLayout.setOnRefreshListener(refreshLayout -> StudentManager.getInstance().refresh(MainActivity.this));
+        smartRefreshLayout.setOnLoadMoreListener(refreshLayout -> StudentManager.getInstance().loadMore(MainActivity.this));
     }
 
     @Override
     public void onRefreshFail(String reason) {
         Toast.makeText(this,reason,Toast.LENGTH_SHORT).show();
+        if (smartRefreshLayout != null) {
+            smartRefreshLayout.finishRefresh();
+        }
     }
 
     @Override
@@ -46,17 +55,26 @@ public class MainActivity extends AppCompatActivity  implements ILoadStudentList
         if (studentAdapter != null) {
             studentAdapter.refreshData(list);
         }
+        if (smartRefreshLayout != null) {
+            smartRefreshLayout.finishRefresh();
+        }
     }
 
     @Override
     public void onLoadFail(String reason) {
         Toast.makeText(this,reason,Toast.LENGTH_SHORT).show();
+        if (smartRefreshLayout != null) {
+            smartRefreshLayout.finishLoadMore();
+        }
     }
 
     @Override
     public void onLoadSuccess(List<StudentResponse.DataBean> list) {
         if (studentAdapter != null) {
             studentAdapter.loadData(list);
+        }
+        if (smartRefreshLayout != null) {
+            smartRefreshLayout.finishLoadMore();
         }
     }
 }
